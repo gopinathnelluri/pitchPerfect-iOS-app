a61643dd536e4b29a21dda32042f654486f0af4c
Gopinath Nelluri <gopinathnelluri@hotmail.com>
2015-12-19 21:40:30 -0600

    added stop button

--- pitch perfect/ViewController.swift
+++ pitch perfect/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordingInProgress: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -23,8 +25,12 @@ class ViewController: UIViewController {
 
     @IBAction func recordAudio(sender: UIButton) {
         //TODO: Show test "Recording in progress"
+        recordingInProgress.hidden = false;
         //TODO: Record audio
         print("Recording");
+    }
+    @IBAction func stopButton(sender: UIButton) {
+        recordingInProgress.hidden = true;
     }
 }
 
