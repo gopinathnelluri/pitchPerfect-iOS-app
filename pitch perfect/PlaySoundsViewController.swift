97f888558579ab5022975fc21326c737f7b3b847
Gopinath Nelluri <gopinathnelluri@hotmail.com>
2015-12-20 17:02:37 -0600

    added slow and fast rate sound effects

---
+++ pitch perfect/PlaySoundsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  PlaySoundsViewController.swift
+//  pitch perfect
+//
+//  Created by Gopinath Nelluri on 12/20/15.
+//  Copyright © 2015 Gopinath Nelluri. All rights reserved.
+//
+
+import UIKit
+
+class PlaySoundsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
