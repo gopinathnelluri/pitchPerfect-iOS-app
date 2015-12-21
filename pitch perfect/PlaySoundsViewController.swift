7a40b92e51afb2b281f19ad6580fb680c5bd3ad3
Gopinath Nelluri <gopinathnelluri@hotmail.com>
2015-12-20 23:35:59 -0600

    mission acomplished

--- pitch perfect/PlaySoundsViewController.swift
+++ pitch perfect/PlaySoundsViewController.swift
@@ -13,6 +13,8 @@ class PlaySoundsViewController: UIViewController {
 
     var audioPlayer:AVAudioPlayer!
     var receivedAudio:RecordedAudio!
+    var audioEngine:AVAudioEngine!
+    var audioFile:AVAudioFile!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,7 +34,9 @@ class PlaySoundsViewController: UIViewController {
         }
         
         audioPlayer.enableRate = true;
-        
+        audioEngine = AVAudioEngine();
+        audioFile = try! AVAudioFile(forReading: receivedAudio.filePathUrl)
+
         // Do any additional setup after loading the view.
     }
 
@@ -41,7 +45,36 @@ class PlaySoundsViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func chipmunk(sender: UIButton) {
+        playAudioWithVariablePitch(1000)
+    }
 
+    @IBAction func darthvader(sender: UIButton) {
+        playAudioWithVariablePitch(-1000)
+    }
+    
+    func playAudioWithVariablePitch(pitch: Float){
+        audioPlayer.stop()
+        audioEngine.stop()
+        audioEngine.reset()
+        
+        let audioPlayerNode = AVAudioPlayerNode()
+        audioEngine.attachNode(audioPlayerNode)
+        
+        let changePitchEffect = AVAudioUnitTimePitch()
+        changePitchEffect.pitch = pitch
+        audioEngine.attachNode(changePitchEffect)
+        
+        audioEngine.connect(audioPlayerNode, to: changePitchEffect, format: nil)
+        audioEngine.connect(changePitchEffect, to: audioEngine.outputNode, format: nil)
+        
+        audioPlayerNode.scheduleFile(audioFile, atTime: nil, completionHandler: nil)
+        try! audioEngine.start()
+        
+        audioPlayerNode.play()
+    }
+    
+    
     @IBAction func snail(sender: UIButton) {
         audioPlayer.stop();
         audioPlayer.rate = 0.5;
